61f6de19041192152794a371a6293627a3ea4c5e
SamWij <sam.wijesinha@gmail.com>
2017-09-02 11:22:07 +1000

    split result into a array

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
-var flatiron = require('flatiron'),
-    app = flatiron.app;
+const flatiron = require('flatiron'),
+  app = flatiron.app;
+  path = require('path');
+
+//to start the app: node app.js pacman
 
 app.use(flatiron.plugins.cli, {
-  dir: __dirname,
+  dir: path.join(__dirname, 'cmds'),
+
   usage: [
-    'hello - say hello to somebody.'
+    'Welcome to Pacman',
+    'Your command didn\'t do anything.',
+    'This is expected.'
   ]
+
 });
 
 app.cmd('pacman', function () {
+  // PLACE X,Y,F
+  // MOVE
+  // LEFT
+  // RIGHT
+  // REPORT
+
   const board = [
   "0,0","0,1","0,2", "0,3", "0,4",
   "1,0","1,1","1,2", "1,3", "1,4",
   "2,0","2,1","2,2", "2,3", "2,4",
   "3,0","3,1","3,2", "3,3", "3,4",
-  "4,0","4,1","4,2", "4,3", "4,4"]
+  "4,0","4,1","4,2", "4,3", "4,4"];
 
-  app.log.info('Welcome to Pacman!  Please Place Pacman on the board by entering a command: PLACE X,Y,F');
+  const location = ["NORTH", "SOUTH", "EAST", "WEST"];
+  const step = ["MOVE", "LEFT", "RIGHT"];
+  const position  = [];
 
+  app.log.info('Welcome to Pacman! \r\n Move Pacman on the board by entering a command:\r\n PLACE X,Y,F');
 
+  app.prompt.get('move', function (err, result) {
 
-  app.prompt.get('ENTER ', function (err, result) {
-    app.log.info('hello '+result.name+'!');
+    var nameList = result.move.split(/[,\s]+|[,\s]+/g);
+
+      app.log.info(nameList);
+    // if (result.command === "END") {
+    //   app.log.info("you entered END");
+    // }
   })
 })
+
+
+
 
 app.start();
