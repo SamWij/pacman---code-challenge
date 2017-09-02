090879196987d5d2963af49e1df3a50273fbc4c0
SamWij <sam.wijesinha@gmail.com>
2017-09-02 10:06:55 +1000

    added board

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var flatiron = require('flatiron'),
 app.use(flatiron.plugins.cli, {
   dir: __dirname,
   usage: [
-    'This is a basic flatiron cli application example!',
-    '',
     'hello - say hello to somebody.'
   ]
 });
 
 app.cmd('pacman', function () {
+  const board = [
+  "0,0","0,1","0,2", "0,3", "0,4",
+  "1,0","1,1","1,2", "1,3", "1,4",
+  "2,0","2,1","2,2", "2,3", "2,4",
+  "3,0","3,1","3,2", "3,3", "3,4",
+  "4,0","4,1","4,2", "4,3", "4,4"]
+
   app.log.info('Welcome to Pacman!  Please Place Pacman on the board by entering a command: PLACE X,Y,F');
+
+
 
   app.prompt.get('ENTER ', function (err, result) {
     app.log.info('hello '+result.name+'!');
