6b76d9a48b8ebcd00046fb3aaff58815d788dea3
SamWij <sam.wijesinha@gmail.com>
2017-09-03 17:31:56 +1000

    added move calculations based on move

--- pacman.js
+++ pacman.js
@@ -10,32 +10,54 @@ const board = [
   "4,0","4,1","4,2", "4,3", "4,4"];
 
 function placePacmanOnBoard() {
-  var placePacman = readlineSync.question('Place Pacman on the Board e.g. PLACE 1,0,NORTH: ');
+  var placePacman = readlineSync.question('Place Pacman on the Board e.g. 1,0,NORTH: ');
   validatePlacement(placePacman);
 };
 
-function movePacman(place, xyPosition, directionFacing) {
-  var movePacman = readlineSync.question('Move Pacman forward (MOVE), left (LEFT), right (RIGHT): ');
+function movePacman(x,y, directionFacing) {
+  var movePacmanOnBoard = readlineSync.question('Move Pacman forward (MOVE), left (LEFT), right (RIGHT): ');
+  movePacmanOnBoard = movePacmanOnBoard.toUpperCase()
+  console.log(movePacmanOnBoard)
+
+  if (movePacmanOnBoard === "MOVE") {
+
+    y = y++
+    directionFacing = directionFacing
+  } else if (movePacmanOnBoard === "LEFT") {
+
+    x = x-1
+    directionFacing = "WEST"
+  } else if (movePacmanOnBoard === "RIGHT"){
+  
+    y = y-1
+    directionFacing = "EAST"
+  }
+
+  console.log("NEW DIRECTION: " + x + "," + y + " - " + directionFacing)
+  movePacman(x,y,directionFacing)
+
 };
 
 
 function validatePlacement(placePacman) {
   var positionArray = placePacman.split(/[,\s]+|[,\s]+/g);
   positionArray.map(function(x){ return x.toUpperCase() });
-  var place = positionArray[0]
   var xyPosition = positionArray[1]+","+positionArray[2]
+  var x = parseInt(positionArray[1])
+  var y = parseInt(positionArray[2])
   var directionFacing = positionArray[3]
 
-  if ( place === "PLACE" && board.includes(xyPosition) && direction.includes(directionFacing)) {
+  if (board.includes(xyPosition) && direction.includes(directionFacing)) {
 
-    movePacman(place, xyPosition, directionFacing);
+    movePacman(x, y, directionFacing);
 
   } else {
       console.log("Error, please enter a valid place")
       placePacmanOnBoard();
   }
 
-  console.log('Pacman Placed: ' + positionArray);
+
+
 }
 
 placePacmanOnBoard();
