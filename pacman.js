29be5b7c3f5314d3112bb8422e721e7dcea6fa32
SamWij <sam.wijesinha@gmail.com>
2017-09-03 20:00:41 +1000

    moved move validation function to another file

--- pacman.js
+++ pacman.js
@@ -1,5 +1,5 @@
 const readlineSync = require('readline-sync');
-
+const pacman = require('./lib/Move');
 const direction = ["NORTH", "EAST", "SOUTH", "WEST"];
 const move = ["MOVE", "LEFT", "RIGHT"];
 const board = [
@@ -18,95 +18,7 @@ function movePacman(x,y, directionFacing) {
   var movePacmanOnBoard = readlineSync.question('Move Pacman forward (MOVE), left (LEFT), right (RIGHT): ');
   movePacmanOnBoard = movePacmanOnBoard.toUpperCase()
 
-  if (directionFacing === "NORTH"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": x = x + 1;
-      break;
-    case "LEFT": x = x - 1;
-      break;
-    case "MOVE": y = y + 1;
-      break;
-    }
-  } // Close North
-  else if (directionFacing === "EAST"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": y = y + 1;
-      break;
-    case "LEFT": y = y - 1;
-      break;
-    case "MOVE": x = x + 1;
-      break;
-    }
-  } //Close East
-  else if (directionFacing === "WEST"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": y = y + 1;
-      break;
-    case "LEFT": y = y - 1;
-      break;
-    case "MOVE": x = x - 1;
-      break;
-    }
-  } //Close West
-  else if (directionFacing === "SOUTH"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": x = x - 1;
-      break;
-    case "LEFT": x = x + 1;
-      break;
-    case "MOVE": y = y - 1;
-      break;
-    }
-  } //Close South
-  else {
-    console.log("No valid move given");
-  } //Close Else
-
-  if (directionFacing === "NORTH"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": directionFacing = "EAST";
-      break;
-    case "LEFT": directionFacing = "WEST";
-      break;
-    case "MOVE": directionFacing = "NORTH"
-      break;
-    }
-  } // Close North
-  else if (directionFacing === "EAST"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": directionFacing = "SOUTH"
-      break;
-    case "LEFT": directionFacing = "NORTH"
-      break;
-    case "MOVE": directionFacing = "EAST";
-      break;
-    }
-  } //Close East
-  else if (directionFacing === "WEST"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": directionFacing = "NORTH"
-      break;
-    case "LEFT": directionFacing = "SOUTH"
-      break;
-    case "MOVE": directionFacing = "WEST"
-      break;
-    }
-  } //Close West
-  else if (directionFacing === "SOUTH"){
-    switch(movePacmanOnBoard) {
-    case "RIGHT": directionFacing = "WEST"
-      break;
-    case "LEFT": directionFacing = "EAST"
-      break;
-    case "MOVE": directionFacing = "SOUTH";
-      break;
-    }
-  } //Close South
-  else {
-    console.log("No valid move given");
-  } //Close Else
-
-
+  pacman.validateMove(directionFacing, movePacmanOnBoard, x, y)
 
   console.log("NEW DIRECTION: " + x + "," + y + " - " + directionFacing)
   movePacman(x,y,directionFacing)
