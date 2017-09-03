0632e19b6d3d3b19f0423f79332e7dcf17fa6ba0
SamWij <sam.wijesinha@gmail.com>
2017-09-03 20:13:46 +1000

    pacman completed, no board validation

--- pacman.js
+++ pacman.js
@@ -1,5 +1,6 @@
 const readlineSync = require('readline-sync');
-const pacman = require('./lib/Move');
+
+//Set Pacman variables
 const direction = ["NORTH", "EAST", "SOUTH", "WEST"];
 const move = ["MOVE", "LEFT", "RIGHT"];
 const board = [
@@ -9,23 +10,24 @@ const board = [
   "3,0","3,1","3,2", "3,3", "3,4",
   "4,0","4,1","4,2", "4,3", "4,4"];
 
+
+//User enters placement of Pacman on board.
 function placePacmanOnBoard() {
-  var placePacman = readlineSync.question('Place Pacman on the Board e.g. 1,0,NORTH: ');
+  var placePacman = readlineSync.question('Place Pacman on the Board e.g. PLACE 1,0,NORTH: ');
   validatePlacement(placePacman);
 };
-
+//User moves Pacman on board
 function movePacman(x,y, directionFacing) {
   var movePacmanOnBoard = readlineSync.question('Move Pacman forward (MOVE), left (LEFT), right (RIGHT): ');
   movePacmanOnBoard = movePacmanOnBoard.toUpperCase()
 
-  pacman.validateMove(directionFacing, movePacmanOnBoard, x, y)
+  validateMove(directionFacing, movePacmanOnBoard, x, y)
 
-  console.log("NEW DIRECTION: " + x + "," + y + " - " + directionFacing)
   movePacman(x,y,directionFacing)
 
 };
 
-
+//validates the  users entry to determine a valid move
 function validatePlacement(placePacman) {
   var positionArray = placePacman.split(/[,\s]+|[,\s]+/g);
   positionArray.map(function(x){ return x.toUpperCase() });
@@ -35,14 +37,107 @@ function validatePlacement(placePacman) {
   var directionFacing = positionArray[3]
 
   if (board.includes(xyPosition) && direction.includes(directionFacing)) {
-
     movePacman(x, y, directionFacing);
-
   } else {
       console.log("Error, please enter a valid place")
       placePacmanOnBoard();
   }
 
-}
+};
+
+//validates and calculates the users move entered
+function validateMove(directionFacing, movePacmanOnBoard, x, y) {
+  console.log("here")
+  if (directionFacing === "NORTH"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": x = x + 1;
+      break;
+    case "LEFT": x = x - 1;
+      break;
+    case "MOVE": y = y + 1;
+      break;
+    }
+  } // Close North
+  else if (directionFacing === "EAST"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": y = y + 1;
+      break;
+    case "LEFT": y = y - 1;
+      break;
+    case "MOVE": x = x + 1;
+      break;
+    }
+  } //Close East
+  else if (directionFacing === "WEST"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": y = y + 1;
+      break;
+    case "LEFT": y = y - 1;
+      break;
+    case "MOVE": x = x - 1;
+      break;
+    }
+  } //Close West
+  else if (directionFacing === "SOUTH"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": x = x - 1;
+      break;
+    case "LEFT": x = x + 1;
+      break;
+    case "MOVE": y = y - 1;
+      break;
+    }
+  } //Close South
+  else {
+    console.log("No valid move given");
+  } //Close Else
+
+  if (directionFacing === "NORTH"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": directionFacing = "EAST";
+      break;
+    case "LEFT": directionFacing = "WEST";
+      break;
+    case "MOVE": directionFacing = "NORTH"
+      break;
+    }
+  } // Close North
+  else if (directionFacing === "EAST"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": directionFacing = "SOUTH"
+      break;
+    case "LEFT": directionFacing = "NORTH"
+      break;
+    case "MOVE": directionFacing = "EAST";
+      break;
+    }
+  } //Close East
+  else if (directionFacing === "WEST"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": directionFacing = "NORTH"
+      break;
+    case "LEFT": directionFacing = "SOUTH"
+      break;
+    case "MOVE": directionFacing = "WEST"
+      break;
+    }
+  } //Close West
+  else if (directionFacing === "SOUTH"){
+    switch(movePacmanOnBoard) {
+    case "RIGHT": directionFacing = "WEST"
+      break;
+    case "LEFT": directionFacing = "EAST"
+      break;
+    case "MOVE": directionFacing = "SOUTH";
+      break;
+    }
+  } //Close South
+  else {
+    console.log("No valid move given");
+  } //Close Else
+
+  console.log("NEW DIRECTION: " + x + "," + y + " - " + directionFacing)
+
+};
 
 placePacmanOnBoard();
